c2557c7d41a3255a21602076eb7d5814b681f394
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2025-09-06 18:22:23 -0300

    style: small refactor creates config file

---
+++ src/paint_alpha/config.hpp
@@ -0,0 +1,41 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <GL/freeglut_std.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+
+#include <string>
+#include <vector>
+#include <format>
+
+const std::string PROGRAM_NAME = "PAINT_ALPHA";
+const std::string VERSION = "1.0";
+const int WINDOW_LENGTH = 800;
+const int WINDOW_HEIGHT = 640;
+const int PALETTE_SIZE = 50;
+const std::vector<float> BG_COLOR = {0.0, 0.0, 0.0, 0.0};
+
+int init(int argc, char *argv[]) {
+  glutInit(&argc, argv);
+
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+
+  glutInitWindowSize(WINDOW_LENGTH, WINDOW_HEIGHT);
+
+  glutInitWindowPosition(100, 100);
+
+  glutCreateWindow(format("{}_{}", PROGRAM_NAME, VERSION).c_str());
+
+  // TODO: improve this
+  glClearColor(BG_COLOR[0], BG_COLOR[1], BG_COLOR[2], BG_COLOR[3]);
+
+  glMatrixMode(GL_PROJECTION);
+
+  gluOrtho2D(0.0, WINDOW_LENGTH, 0.0, WINDOW_HEIGHT);
+
+  return 0;
+}
+
+#endif
--- src/paint_alpha/main.cpp
+++ src/paint_alpha/main.cpp
@@ -2,37 +2,18 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
-#include <format>
-#include <string>
 
+#include "config.hpp"
 #include "palette.hpp"
 
-int init(void);
 void display(void);
 void handle_keyboard(GLubyte key, GLint x, GLint y);
 void handle_motion(GLint x, GLint y);
 
-// TODO: move this somewhere better 
-const std::string PROGRAM_NAME = "PAINT_ALPHA";
-const std::string VERSION = "1.0";
-const int WINDOW_LENGTH = 800;
-const int WINDOW_HEIGHT = 640;
-const int PALETTE_SIZE = 50;
-
 Pallete palette = Pallete(PALETTE_SIZE);
 
 int main(int argc, char *argv[]) {
-  glutInit(&argc, argv);
-
-  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-
-  glutInitWindowSize(WINDOW_LENGTH, WINDOW_HEIGHT);
-
-  glutInitWindowPosition(100, 100);
-
-  glutCreateWindow(std::format("{}_{}", PROGRAM_NAME, VERSION).c_str());
-
-  init();
+  init(argc, argv);
 
   glutDisplayFunc(display);
 
@@ -41,17 +22,6 @@ int main(int argc, char *argv[]) {
   glutMotionFunc(handle_motion);
 
   glutMainLoop();
-
-  return 0;
-}
-
-int init(void) {
-
-  glClearColor(0.0, 0.0, 0.0, 0.0);
-
-  glMatrixMode(GL_PROJECTION);
-
-  gluOrtho2D(0.0, WINDOW_LENGTH, 0.0, WINDOW_HEIGHT);
 
   return 0;
 }
