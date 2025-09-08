38309ab0a433d59d6fd6044fec80fcbad63fb29a
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2025-09-07 21:30:17 -0300

    feat: bigger simple points drawn

--- src/paint_alpha/config.hpp
+++ src/paint_alpha/config.hpp
@@ -3,6 +3,7 @@
 
 #include <GL/freeglut_std.h>
 #include <GL/gl.h>
+#include <GL/glext.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
 
@@ -16,6 +17,7 @@ const int WINDOW_LENGTH = 800;
 const int WINDOW_HEIGHT = 640;
 const int PALETTE_SIZE = 50;
 const float PALETTE_SPACING = 1.5;
+const float MY_POINT_SIZE = 8.0;
 const std::vector<float> BG_COLOR = {0.0, 0.0, 0.0, 0.0};
 
 int init(int argc, char *argv[]) {
@@ -28,6 +30,8 @@ int init(int argc, char *argv[]) {
   glutInitWindowPosition(100, 100);
 
   glutCreateWindow(format("{}_{}", PROGRAM_NAME, VERSION).c_str());
+
+  glPointSize(MY_POINT_SIZE);
 
   // TODO: improve this
   glClearColor(BG_COLOR[0], BG_COLOR[1], BG_COLOR[2], BG_COLOR[3]);
